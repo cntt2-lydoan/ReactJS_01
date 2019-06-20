68009b0f0fb0d69a478936a9d456e141f1bc859c
ly <doananhly1999@gmail.com>
2019-06-20 19:42:59 +0700

    [PHAN-02] 006 Dinh nghia component cach so 1

---
+++ a1/src/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function One(){
+  return (
+  <div>
+    <h2>So 1 tap 1</h2>
+    <h3>So 1 tap 2 </h3>
+  </div> 
+  )
+}
+var Two =function(){
+  return(
+    <div>
+      <h3>Cach so 2</h3>
+    </div>
+  )
+}; 
+var Three = () =>(
+                  <div>
+                  <h3>Cach so 3</h3>
+                  </div>
+                );
+                
+class Four extends Component {
+  render() {
+    return (
+      <div>
+        <button>React js component so 4</button>
+      </div>
+    );
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <One/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Two/>
+      <Three/>
+      <hr/>
+      <Four/>
+    </div>
+  );
+}
+
+export default App;
