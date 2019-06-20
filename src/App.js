fdc099c9270622718942d4ca80392dfdb670a0a2
ly <doananhly1999@gmail.com>
2019-06-21 02:26:42 +0700

    [PHAN-03] Khai niem va cach truyen thong tin qua props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//Su dung cach 1(funtion)
+function NumberOne(props){
+  return(
+    <div className="col-6">
+      <div className="card">
+          <img className="card-img-top" src={props.linkanh} alt=""></img>
+          <div className="card-body">
+            <h4 className="card-title">{props.tieude}</h4>
+            <p className="card-text">Text</p>
+          </div>
+      </div>
+
+    </div>
+  )
+} 
+
+//Cach 2 su dung Class de dinh nghia va thao tac voi props
+class NumberTwo extends Component {
+  render() {
+    return (
+      <div className="col-4">
+      <div className="card">
+          <img className="card-img-top" src={this.props.linkanh} alt=""></img>
+          <div className="card-body">
+            <h4 className="card-title">{this.props.tieude}</h4>
+            <p className="card-text">Lorem ipsum dolor...................</p>
+          </div>
+      </div>
+
+    </div>
+    );
+  }
+}
+
 
 function App() {
   return (
@@ -19,6 +54,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="container">
+          <div className="row">
+              <NumberOne tieude="San pham so 1" linkanh="https://png.pngtree.com/element_pic/17/03/02/740c21696ef195717fa3ab5140851638.jpg"/>
+              <NumberOne tieude="Aquafina" linkanh="https://img.websosanh.vn/v2/users/root_product/images/nuoc-uong-dong-chai-aquafina/XSkUPf4CgrR5.jpg?compress=85&width=220"/>
+              <NumberTwo tieude="THtrue milk" linkanh="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVUvV1KU-_gz6GEAwWy9UQKNE9arp12IlDLWAbXmhIMKcy3nIDsg"/>
+          
+          </div>
+      </div>
+
     </div>
   );
 }
