04738f28e620d3f15138f313b6243f6480ccc2d1
ly <doananhly1999@gmail.com>
2019-06-21 12:43:41 +0700

    [PHAN-04] Thuc hanh jsx va map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const a1="ly";
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Ten toi la: {a1}</h3>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" name id defaultValue="checkedValue" defaultChecked />
+            Display value
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+//Vi du ve ham Map
+const so=[1,2,3,4,5,6,7,8];
+const so2= so.map((x) => x*2+",");
+const so3= so.map((x) => (
+  <li>so: {x} </li>
+));
+class App2 extends Component {
+  render() {
+    return (
+      <div>
+        {so3}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <App2/>
         <a
           className="App-link"
           href="https://reactjs.org"
