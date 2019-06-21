d8a5dd976ba476df582a2dafa72ab7f913e43a9a
ly <doananhly1999@gmail.com>
2019-06-21 23:07:46 +0700

    [PHAN-05] Component hoa giao dien html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopMenu from './Component/topmenu/TopMenu.js';
+import Header from './Component/Header/Header.js';
+import Content from './Component/Content/Content.js';
+import Footer from './Component/Footer/Footer.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="_013">
+      <TopMenu/>
+      <Header/>
+      <Content tieude="Cach su dung props bang class" vitri1="order-lg-2" anh="img/01.jpg" trichdan="Day la trich dan so 1"/>
+      <Content tieude="Tin tuc so 2" anh="img/02.jpg" trichdan="Day la trich dan so 2"/>
+      <Content tieude="Tin tuc so 3" vitri1="order-lg-2" anh="img/03.jpg" trichdan="Day la trich dan so 3"/>
+      <Footer/>
     </div>
   );
 }
